dc28346f3c2453e59d15ec546db73d9db70e849f
Serhii Rubets <serhii_rubets@epam.com>
2018-01-15 17:53:24 +0200

    Changed environment for starting the project

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -4,7 +4,6 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   button: {
     width: 100,
-    color: 'black',
     borderColor: 'black',
     borderWidth: 1,
     paddingLeft: 10,
