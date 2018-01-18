ca969e4e26e4c721bd0d9ed700122f6896c98cf9
Serhii Rubets <serhii_rubets@epam.com>
2018-01-18 10:46:47 +0200

    React-native-navigation

--- App.js
+++ App.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Navigation } from 'react-native-navigation';
+
+import { registerScreens } from './screens';
+
+registerScreens(); // this is where you register all of your app's screens
+
+// start the app
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: 'One',
+      screen: 'example.FirstTabScreen', // this is a registered name for a screen
+      icon: require('../img/one.png'),
+      selectedIcon: require('../img/one_selected.png'), // iOS only
+      title: 'Screen One'
+    },
+    {
+      label: 'Two',
+      screen: 'example.SecondTabScreen',
+      icon: require('../img/two.png'),
+      selectedIcon: require('../img/two_selected.png'), // iOS only
+      title: 'Screen Two'
+    }
+  ]
+});
+
 import LoginForm from './src/components/login-form';
+
+// register all screens of the app (including internal ones)
+export function registerScreens() {
+  Navigation.registerComponent('example.FirstTabScreen', () => LoginForm);
+  Navigation.registerComponent('example.SecondTabScreen', () => LoginForm);
+  Navigation.registerComponent('example.PushedScreen', () => LoginForm);
+}
 
 export default class App extends React.Component {
   render() {
--- ios/shop/AppDelegate.m
+++ ios/shop/AppDelegate.m
@@ -31,6 +31,22 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+#ifdef DEBUG
+  //  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+  
+  
+  // **********************************************
+  // *** DON'T MISS: THIS IS HOW WE BOOTSTRAP *****
+  // **********************************************
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  self.window.backgroundColor = [UIColor whiteColor];
+  [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation launchOptions:launchOptions];
+  
   return YES;
 }
 
