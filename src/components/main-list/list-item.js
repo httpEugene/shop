42cdd7d9b4b914a649786b71d9695f4a97b6b651
Svitlana <sinergy.ua@gmail.com>
2018-01-19 16:47:08 +0200

    List item has been added.

--- src/components/main-list/list-item.js
+++ src/components/main-list/list-item.js
@@ -1,55 +1,57 @@
-import React, { PureComponent } from 'react';
-import { Actions } from 'react-native-router-flux';
-import { connect } from 'react-redux';
-import Field from '../field';
-import Button from '../button';
-import styles from './styles';
+import React, { PureComponent } from 'react'
+import { Actions } from 'react-native-router-flux'
+import { connect } from 'react-redux'
+import Field from '../field'
+import Button from '../button'
+import styles from './styles'
 import {
-    View,
-    Text,
-    TextInput,
-    TouchableHighlight,
-    TouchableOpacity,
-    AsyncStorage
-} from 'react-native';
+  View,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  AsyncStorage
+} from 'react-native'
 
-function mapStateToProps() {
-  return {};
+function mapStateToProps () {
+  return {}
 }
 
-function mapDispatchToProps(dispatch) {
-  return {};
+function mapDispatchToProps (dispatch) {
+  return {}
 }
 
 @connect(mapStateToProps, mapDispatchToProps)
 
 export default class ListItem extends PureComponent {
-        getDeliveryDetails = () => {
-            Actions.deliveryDetails();
-        }
+  getDeliveryDetails = () => {
+    Actions.deliveryDetails();
+  }
 
-        getDetails = () => {
-           // Actions.details();
-        }
+  getDetails = () => {
+    // Actions.details();
+  }
 
-          render() {
-            return (
-              <View style={styles.listItemContainer}>
-                 <Text style={styles.id}>id</Text>
-                 <Text style={styles.date}>date</Text>
+  render () {
+    return (
+      <View style={styles.listItemContainer}>
+        <Text style={styles.id}>id</Text>
+        <Text style={styles.date}>date</Text>
 
-                 <View style={styles.statusContainer}>
-                    <Text style={styles.status}>date</Text>
-                 </View>
-                 <View style={styles.purchaseContainer}>
-                    <Text style={styles.purchaseItem}>item</Text>
-                    <Text style={styles.purchaseSumm}>Summ</Text>
-                 </View>
-                 <View style={styles.buttonContainer}>
-                     <Button styleName='details' styleTextName='buttonTextSecond' label='Details' onPressHandler={() => this.getDetails()} />
-                     <Button styleName='deliveryDetails' styleTextName='buttonTextSecond' label='Delivery Details' onPressHandler={() => this.getDeliveryDetails()} />
-                 </View>
-              </View>
-            );
+        <View style={styles.statusContainer}>
+          <Text style={styles.status}>date</Text>
+        </View>
+        <View style={styles.purchaseContainer}>
+          <Text style={styles.purchaseItem}>item</Text>
+          <Text style={styles.purchaseSumm}>Summ</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button styleName='details' styleTextName='buttonTextSecond' label='Details'
+                  onPressHandler={() => this.getDetails()}/>
+          <Button styleName='deliveryDetails' styleTextName='buttonTextSecond' label='Delivery Details'
+                  onPressHandler={() => this.getDeliveryDetails()}/>
+        </View>
+      </View>
+    )
   }
 }
