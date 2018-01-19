da3a531148ecd58f13ba18214f1508eac9adf391
Serhii Rubets <serhii_rubets@epam.com>
2018-01-19 18:16:20 +0200

    Add config for thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,29 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+
+const enhancers = [];
+const middleware = [
+  thunk,
+];
+
+const devToolsExtension = window.devToolsExtension;
+
+if (devToolsExtension) {
+  enhancers.push(devToolsExtension());
+}
+
+const initialState = {};
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers,
+);
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  initialState,
+  composedEnhancers,
 );
 
 export default store;
