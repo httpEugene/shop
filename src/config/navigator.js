3b043bb06399a015a4a00d6223eaf9865ae69193
Svitlana <sinergy.ua@gmail.com>
2018-01-23 14:21:40 +0200

    Lint problem has been fixed.

--- src/config/navigator.js
+++ src/config/navigator.js
@@ -9,29 +9,6 @@ import DeliveryDetails from '../components/delivery-details';
 
 const Second = () => <Text>Second Screen</Text>;
 
-const scenes = [
-  {
-    key: 'login',
-    component: 'LoginForm',
-    title: 'Login',
-  },
-  {
-    key: 'main',
-    component: 'MainOrderList',
-    title: 'My Orders',
-  },
-  {
-    key: 'order',
-    component: 'OrderItem',
-    title: 'Order Details',
-  },
-  {
-    key: 'deliveryDetails',
-    component: 'DeliveryDetails',
-    title: 'Delivery Details',
-  },
-];
-
 const Navigator = () => (
   <Router>
     <Scene key="root">
