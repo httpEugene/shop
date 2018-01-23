774507e2783b726f316ba58623921f13f45a3afc
Serhii Rubets <serhii_rubets@epam.com>
2018-01-23 14:35:16 +0200

    Navigator was changed

--- src/config/navigator.js
+++ src/config/navigator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 import LoginForm from '../components/login-form';
 import MainOrderList from '../components/main-order-list';
@@ -7,41 +6,44 @@ import OrderItem from '../components/order-item';
 
 import DeliveryDetails from '../components/delivery-details';
 
-const Second = () => (
-  <Text>Second Screen</Text>
-);
+const scenes = [
+  {
+    key: 'login',
+    component: LoginForm,
+    title: 'Login',
+  },
+  {
+    key: 'main',
+    component: MainOrderList,
+    title: 'My Orders',
+  },
+  {
+    key: 'order',
+    component: OrderItem,
+    title: 'Order Details',
+  },
+  {
+    key: 'deliveryDetails',
+    component: DeliveryDetails,
+    title: 'Delivery Details',
+  },
+];
+
+const initialScene = 0;
+const scenesComponent = initialSceneNumber =>
+  scenes.map((el, index) => {
+    if (index === initialSceneNumber) {
+      return (
+        <Scene key={el.key} component={el.component} title={el.type} initial />
+      );
+    }
+
+    return <Scene key={el.key} component={el.component} title={el.type} />;
+  });
 
 const Navigator = () => (
   <Router>
-    <Scene key="root">
-      <Scene
-        key="login"
-        component={LoginForm}
-        title="Login"
-        initial
-      />
-      <Scene
-        key="gray"
-        component={Second}
-        title="Gray"
-      />
-       <Scene
-          key="main"
-          component={MainOrderList}
-          title="My Orders"
-        />
-        <Scene
-          key="order"
-          component={OrderItem}
-          title="Order Details"
-        />
-
-        <Scene
-          key="deliveryDetails"
-          component={DeliveryDetails}
-          title="Delivery Details"
-        />
-    </Scene>
+    <Scene key="root">{scenesComponent(initialScene)}</Scene>
   </Router>
 );
 
