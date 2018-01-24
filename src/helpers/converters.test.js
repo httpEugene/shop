efbbe42fa361d863adf06b5e13a0800092265f3e
Gennadii Vodianytskyi <gennadii_vodianytskyi@epam.com>
2018-01-24 19:08:23 +0200

    some changes after review

---
+++ src/helpers/converters.test.js
@@ -0,0 +1,10 @@
+import convertDateToShortUTC from '../converters';
+
+describe('convertDateToShortUTC', () => {
+  test('should convert date in format YYYY-MM-DD to short UTC format', () => {
+    const date = '2018-01-24';
+    const expectedValue = '24 Jan 2018';
+
+    expect(convertDateToShortUTC(date).toEqual(expectedValue));
+  });
+});
