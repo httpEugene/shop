9e6c3c13c95eb04c28457989c43119d08e277335
Svitlana <sinergy.ua@gmail.com>
2018-01-30 13:15:47 +0200

    Conflicts has been resolved.

--- src/components/main-order-list/actions/orderActions.test.js
+++ src/components/main-order-list/actions/orderActions.test.js
@@ -1,7 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import fetchMock from 'fetch-mock';
-import expect from 'expect';
 import * as actions from './orderActions';
 import { GET_ORDERS, LOADING_FAILED } from '../constants';
 
