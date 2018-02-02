3d82e87485e1aadf1296e5662e94b0eec6121ebb
Serhii Rubets <serhii_rubets@epam.com>
2018-02-02 15:44:41 +0200

    Lost styles were added

---
+++ src/components/loader/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../config/colors';
+
+export default StyleSheet.create({
+  container: {
+    display: 'flex',
+    backgroundColor: colors.secondColorWhite,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loader: {},
+});
