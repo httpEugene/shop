40d88ee4fab3a7d3329ebc38ddf2b9beea2415a6
sergey <sramator@gmail.com>
2018-02-07 13:02:51 +0200

    Settings for push notifications ios were added

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.awesomeproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wix.autogrowtextinput.AutoGrowTextInputPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AutoGrowTextInputPackage(),
           new MapsPackage(),
           new VectorIconsPackage(),
           new ReactNativePushNotificationPackage()
